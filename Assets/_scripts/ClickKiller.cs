d93142240611d5dfdf77eaaa5631ece0117bec2a
Mauro Vanetti <posta@maurovanetti.info>
2017-04-01 17:41:05 +0200

    Click curtains management

---
+++ Assets/_scripts/ClickKiller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickKiller : MonoBehaviour {
+
+    private static List<GameObject> curtainsList = new List<GameObject>();
+    public GameObject curtains;
+
+    void Awake()
+    {
+        if (curtainsList.Contains(curtains))
+        {
+            Debug.Log("Extra curtains needed for " + gameObject.name);
+            GameObject oldCurtains = curtains;
+            curtains = Instantiate(oldCurtains, oldCurtains.transform.parent);         
+            curtains.name = oldCurtains.name + " #" + (curtainsList.Count + 1);
+        }
+        curtainsList.Add(curtains);
+    }
+
+	void Start () {
+    }
+	
+	void Update () {
+		
+	}
+
+    void OnEnable()
+    {
+        Debug.Log(gameObject.name + " ClickKiller.OnEnable()");
+        if (curtains != null)
+        {
+            curtains.SetActive(true);
+        }
+    }
+
+    void OnDisable()
+    {
+        Debug.Log(gameObject.name + " ClickKiller.OnDisable()");
+        if (curtains != null)
+        {
+            curtains.SetActive(false);
+        }
+    }
+}
