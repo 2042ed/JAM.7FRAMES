69f3ee6406d90c72543676bf75780a73350d69fc
Stefano Cecere <stefano.cecere@krur.com>
2017-05-08 07:02:06 +0200

    navigation arrow

---
+++ Assets/_scripts/NavigationArrow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationArrow : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
+            // we are on mobile device
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        } else {
+            // we are in desktop device
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+        }
+    }
+
+}
