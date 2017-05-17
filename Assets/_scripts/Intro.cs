dda2903cf9d414a4e4a48679625c1f0c2b76d977
Stefano Cecere <stefano.cecere@krur.com>
2017-05-17 06:05:03 +0200

    Intro

---
+++ Assets/_scripts/Intro.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Fungus;
+public class Intro : MonoBehaviour
+{
+
+    public Flowchart HomeFlowchart;
+
+    public void OnAnimationFInished()
+    {
+        //HomeFlowchart.BroadcastMessage("AnimationFinished");
+        //HomeFlowchart.SendMessage(, SendMessageOptions.DontRequireReceiver);
+        HomeFlowchart.ExecuteBlock("Intro  finished");
+    }
+}
