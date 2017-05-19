72863cab3564a0efb4fb75224c8c16d1194b19a9
Stefano Cecere <stefano.cecere@krur.com>
2017-05-19 12:08:45 +0200

    credits buoni ora

---
+++ Assets/_scripts/UI/Slideshow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Slideshow : MonoBehaviour
+{
+    public float changeTime = 10.0f;
+    public Sprite[] Slides;
+
+    int currentSlide = 0;
+    float timeSinceLast = 1.0f;
+    Image myImg;
+
+    void Awake()
+    {
+        myImg = gameObject.GetComponent<Image>();
+    }
+
+    void Start()
+    {
+        ChangeSlide();
+    }
+
+    void ChangeSlide()
+    {
+        if (currentSlide >= Slides.Length) {
+            currentSlide = 0;
+        }
+        myImg.sprite = Slides[currentSlide];
+    }
+
+    void Update()
+    {
+        if (timeSinceLast > changeTime) {
+            timeSinceLast = 0.0f;
+            currentSlide++;
+            ChangeSlide();
+            // Debug.Log("inage " + currentSlide);
+        }
+        timeSinceLast += Time.deltaTime;
+    }
+}
