2faa87914d1a8b3d96bf3a884717ddf896776321
Stefano Cecere <stefano.cecere@krur.com>
2017-05-27 21:10:52 +0200

    restored NPC

---
+++ Assets/_scripts/NPG/Butterfly.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JAMURR
+{
+    public class Butterfly : MonoBehaviour
+    {
+        Vector3 target;
+        float timer;
+        int sec;
+
+        float moveSpeed = 0.1f;
+
+        float origX;
+        float origY;
+
+        float maxX = 1f;
+        float minX = -1f;
+        float maxY = 1f;
+        float minY = -1f;
+
+        void Start()
+        {
+            target = ResetTarget();
+            sec = ResetSec();
+
+            //origX = transform.position.x;
+            //origY = transform.position.y;
+        }
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > sec) {
+                target = ResetTarget();
+                sec = ResetSec();
+            }
+            transform.Translate(target * moveSpeed * Time.deltaTime);
+        }
+
+        Vector3 ResetTarget()
+        {
+            return new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
+        }
+
+        int ResetSec()
+        {
+            timer = 0;
+            return Random.Range(1, 3);
+        }
+
+    }
+}
---
+++ Assets/_scripts/NPG/Dragon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JAMURR
+{
+    public class Dragon : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        public void OnMouseDown()
+        {
+            // Debug.Log("OnPointerClick", gameObject);
+            InventoryManager.I.GetDraghetto();
+            gameObject.SetActive(false);
+        }
+    }
+}
